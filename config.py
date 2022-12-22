26c48c90fda34f15193831f83862f068cb67993d
pvkokhan <p.v.kokhan@gmail.com>
2022-12-22 23:57:40 +0300

    12

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "5639340683:AAFT6k0E1HbvtKPWX31fklQ5pR91DArce90"
-api_key = "sk-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"
+api_key = "ssk-78AXPVBOReuIIYIqlN8oT3BlbkFJN2jY8yK1Kxm7uOGnVt12"
 start_msg = "Hello! I am a bot that can generate responses to your questions and requests. Just send me a message and I will try my best to help you."
 date_time_format = "%Y-%m-%d %H:%M:%S"
 separator = "##################################################"
